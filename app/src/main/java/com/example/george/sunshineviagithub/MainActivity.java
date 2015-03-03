f2882be2d6652daa65fd08d7b85885193242df10
george <ggeorgalas@gmail.com>
2015-03-03 16:53:36 -0500

    commit test

--- app/src/main/java/com/example/george/sunshineviagithub/MainActivity.java
+++ app/src/main/java/com/example/george/sunshineviagithub/MainActivity.java
@@ -1,15 +1,15 @@
 package com.example.george.sunshineviagithub;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+//test
+
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 
 
 public class MainActivity extends ActionBarActivity {
